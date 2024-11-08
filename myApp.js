fd33a39c6ebd40882e6f9879ef224dff1aa179c5
Dogukan Kaan Bozkurt <dkaanbozkurt@gmail.com>
2024-11-08 10:24:51 +0100

    Start a Working Express Server

--- myApp.js
+++ myApp.js
@@ -1,6 +1,13 @@
 let express = require('express');
 let app = express();
 
+console.log('Hello World');
+
+// Start a Working Express Server
+app.get('/',(req,res) =>
+{
+    res.send('Hello Express');
+})
 
 
 
@@ -32,8 +39,4 @@ let app = express();
 
 
 
-
-
-
-
- module.exports = app;
+module.exports = app;
