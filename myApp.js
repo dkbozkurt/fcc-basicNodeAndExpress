ec2bf19ceed108e407263ba40706c2d57164c827
Dogukan Kaan Bozkurt <dkaanbozkurt@gmail.com>
2024-11-08 10:28:06 +0100

    Serve an HTML File

--- myApp.js
+++ myApp.js
@@ -4,12 +4,18 @@ let app = express();
 console.log('Hello World');
 
 // Start a Working Express Server
+// app.get('/',(req,res) =>
+// {
+//     res.send('Hello Express');
+// })
+
+// Serve an HTML File
+let targetFilePath = __dirname + '/views/index.html';
+
 app.get('/',(req,res) =>
 {
-    res.send('Hello Express');
+    res.sendFile(targetFilePath);
 })
-
-
 
 
 
